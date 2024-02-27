93f3f856f5ed34b2757436f3378481fc14a85eda
谭凯中 <13131896+tan-kaizhong@user.noreply.gitee.com>
2024-02-27 22:54:29 +0800

    database design

---
+++ db/seq.js
@@ -0,0 +1,29 @@
+const { Sequelize } = require("sequelize");
+
+//用户数据配置
+const {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USER,
+  MYSQL_PWD,
+  MYSQL_DB,
+} = require("../config/config.default");
+// console.log(MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PWD, MYSQL_DB);
+
+const seq = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PWD, {
+  host: MYSQL_HOST,
+  dialect: "mysql", // 选择你使用的数据库类型
+});
+
+seq
+  .authenticate()
+  .then(() => {
+    console.log("数据库连接成功");
+  })
+  .catch((err) => {
+    console.log("数据库连接失败", err);
+  });
+// 自动创建数据库表
+
+
+module.exports = seq;
